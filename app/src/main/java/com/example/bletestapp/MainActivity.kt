ff03a57c445737a198fb8a68917e8498ed7892c1
Kozo Chikai <kchikai@sonix.asia>
2023-04-24 13:08:02 +0900

    add the feature to scan BLE devices

--- app/src/main/java/com/example/bletestapp/MainActivity.kt
+++ app/src/main/java/com/example/bletestapp/MainActivity.kt
@@ -8,12 +8,17 @@ import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     // BLE adapter
     private var mBluetoothAdapter: BluetoothAdapter? = null
+    private var mDeviceAddress = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,12 +63,46 @@ class MainActivity : AppCompatActivity() {
                 finish()
                 return
             }
+            REQUEST_CONNECT_DEVICE -> {
+                val strDeviceName: String?
+                if (RESULT_OK == resultCode) {
+                    // デバイスリストアクティビティからの情報の取得
+                    strDeviceName = data?.getStringExtra(DeviceListActivity.EXTRAS_DEVICE_NAME)!!
+                    mDeviceAddress = data?.getStringExtra(DeviceListActivity.EXTRAS_DEVICE_ADDRESS)!!
+                } else {
+                    strDeviceName = ""
+                    mDeviceAddress = ""
+                }
+                (findViewById<View>(R.id.textview_devicename) as TextView).text =
+                    strDeviceName
+                (findViewById<View>(R.id.textview_deviceaddress) as TextView).text =
+                    mDeviceAddress
+            }
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    // オプションメニュー作成時の処理
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main, menu)
+        return true
+    }
+
+    // オプションメニューのアイテム選択時の処理
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuitem_search -> {
+                val deviceListActivityIntent = Intent(this, DeviceListActivity::class.java)
+                startActivityForResult(deviceListActivityIntent, REQUEST_CONNECT_DEVICE)
+                return true
+            }
+        }
+        return false
     }
 
     companion object {
         // BLE Code to request BLE feature
         private const val REQUEST_ENABLE_BLUETOOTH = 1
+        private const val REQUEST_CONNECT_DEVICE = 2 // デバイス接続要求時の識別コード
     }
 }
